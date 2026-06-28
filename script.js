b57bae33a55b54983ed5f5bce9ea1ddb3180554f
anjalijaiswal4661 <161955948+anjalijaiswal4661@users.noreply.github.com>
2026-06-28 20:06:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function isSameType(value1, value2) {
   //your js code here
+	let result=falsee
 	if((typeof(value1))===(typeof(value2))
 	   {
-		let result=return true;
+		 result=return true;
 	   }
+	
 	
 	
 }
